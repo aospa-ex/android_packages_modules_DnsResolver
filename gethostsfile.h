Spark-Rom/packages_modules_DnsResolver

Toggle-able adblock hosts file [1/3]

gethostsfile.h
@@ -1,0 +1,4 @@
+#define SPARK_PATH_ADBLOCK_HOSTS "/system/etc/hosts.spark_adblock"
+#define SPARK_HOSTS_SETTING_PROP "persist.spark.hosts_block"
+
+const char* gethostsfile();

aospa-ex/android_packages_modules_DnsResolver

Change-Id: Id4c26096dad229e38fc7e51b4d100d9740325d9e

gethostsfile.h
@@ -1,0 +1,4 @@
+#define AOSPA_PATH_ADBLOCK_HOSTS "/system/etc/hosts.aospa_adblock"
+#define AOSPA_HOSTS_SETTING_PROP "persist.aospa.hosts_block"
+
+const char* gethostsfile();